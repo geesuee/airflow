16ad157d0faffec0af5b3fed3cd53a3169621d73
geesuee <baejisu97@gmail.com>
2023-12-05 22:15:00 +0900

    op_args 테스트 dag 업로드

--- dags/dags_python_task_decorator.py
+++ dags/dags_python_task_decorator.py
@@ -9,9 +9,9 @@ with DAG(
     start_date=pendulum.datetime(2023, 3, 3, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
-  
-  @task(task_id="python_task_1")
-  def print_context(some_input):
-      print(some_input)
 
-  python_task_1 = print_context("task_decorator 실행")
+    @task(task_id="python_task_1")
+    def print_context(some_input):
+        print(some_input)
+
+    python_task_1 = print_context("task_decorator 실행")
