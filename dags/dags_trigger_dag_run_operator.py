a17f6378c8c71ae1fec30aa51bdb30012d50837b
geesuee <baejisu97@gmail.com>
2024-05-02 21:02:57 +0900

    TriggerDagRun Operator 활용 DAG 업로드

---
+++ dags/dags_trigger_dag_run_operator.py
@@ -0,0 +1,31 @@
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
+with DAG(
+  dag_id='dags_python_with_trigger_rule_eg2',
+  schedule='30 9 * * *',
+  start_date=pendulum.datetime(2023, 12, 10, tz='Asia/Seoul'),
+  catchup=False
+) as dag:
+    
+    start_task = BashOperator(
+        task_id='start_task',
+        bash_command='echo "start!"'
+    )
+
+    trigger_dag_task = TriggerDagRunOperator(
+        task_id='trigger_dag_task',             # 필수 값
+        trigger_dag_id='dags_python_operator',  # 필수 값, 어떤 DAG을 trigger 할건지
+        trigger_run_id=None,
+        execution_date='{{data_interval_end}}',
+        reset_dag_run=True,
+        wait_for_completion=False,
+        poke_interval=60,
+        allowed_states=['success'],
+        failed_states=None
+    )
+
+    start_task >> trigger_dag_task
