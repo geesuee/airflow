184de21bce63376760577dc657a6a7176dcc390a
geesuee <baejisu97@gmail.com>
2024-05-02 21:08:13 +0900

    TriggerDagRun Operator 활용 DAG 수정

--- dags/dags_trigger_dag_run_operator.py
+++ dags/dags_trigger_dag_run_operator.py
@@ -5,7 +5,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 with DAG(
-  dag_id='dags_python_with_trigger_rule_eg2',
+  dag_id='dags_trigger_dag_run_operator',
   schedule='30 9 * * *',
   start_date=pendulum.datetime(2023, 12, 10, tz='Asia/Seoul'),
   catchup=False
